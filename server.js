06fe28f50786ab2a9897431d026a7956547033e7
Erick <erickdamata@gmail.com>
2020-03-15 20:34:31 -0300

    estrutura de servidor em andamento

--- server.js
+++ server.js
@@ -7,5 +7,22 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 
 app.get('/teste', (req, res) => {res.send('Ok')})
+
+/* app.get('/relatorio', (req, res) => {
+    console.log('recebi')
+    res.send('texto')
+    })  */
+
 const porta = 8080
 app.listen(porta, () => console.log(`Executando na porta ${porta}`))
+
+function retornarTexto() {
+    const fs = require('fs')
+
+    caminho = __dirname + '/relatorio.txt'
+
+    fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    const texto = conteudo
+    return texto
+})
+}
